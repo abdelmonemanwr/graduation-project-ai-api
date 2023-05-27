f9a53847d1523d4db2c82036e6f6d4ebc046a61e
Men3m <abdelmonemanwr7777@gmail.com>
2023-05-27 19:12:34 +0300

    temp azure for testing toxic comment and sentiment

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import json
-import torch
 import pickle
 from flask_cors import CORS
-from collections import Counter
-from transformers import pipeline
 from flask import Flask, request, jsonify
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -54,7 +51,7 @@ def sentiment_analyzer() -> str:
     except Exception as e:
         return {"error": str(e)}, 500
 
-# ////////////////////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////////////
 
 def toxicVec():
     # Load the saved vectorizer
@@ -98,28 +95,6 @@ def toxic_comment_classifier() -> str:
         return result
     except Exception as e:
         return {"error": str(e)}, 500
-
-###########################################################
-
-def count_words(string):
-    freq = Counter(string)
-    return (freq[' '] + 1)
-
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-
-@app.route("/summarize/", methods=["POST"])
-def summarize() -> str:
-    global summarizer
-    text = request.json.get("string")
-    x = count_words(text)
-    Max_Length = int(x / 2)
-    Min_Length = int(x / 4)
-    summerized = summarizer(  text,
-                              max_length=Max_Length,
-                              min_length=Min_Length,
-                              do_sample=False
-                            )
-    return jsonify({"message": summerized})
 
 if __name__ == '__main__':
     app.run(debug=True)
